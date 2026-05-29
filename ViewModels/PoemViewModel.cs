8bc553c600d39da0d7a5c4985315e6627cd00d1d
megaV3 <132799339+megaV3@users.noreply.github.com>
2026-05-29 20:46:49 +0800

    Added new viewmodel

---
+++ ViewModels/PoemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GROUP9PoetryWebsite.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GROUP9PoetryWebsite.ViewModels
+{
+    public class PoemViewModel
+    {
+        public IEnumerable<Poem> Poems { get; set; } // For your feed
+        public Poem Poem { get; set; }               // For the form
+        public IEnumerable<SelectListItem> AnthologyOptions { get; set; }
+
+        // Add this to capture a user-typed title if they aren't using the dropdown
+       
+        public string NewAnthologyTitle { get; set; }
+    }
+}
